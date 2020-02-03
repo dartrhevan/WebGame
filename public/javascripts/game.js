dfbc3129989b4a22dddf7881c67e0a3fb879c514
dartrhevan <vovasatunkin@mail.ru>
2020-02-03 18:44:47 +0500

    simple animation

--- public/javascripts/game.js
+++ public/javascripts/game.js
@@ -6,6 +6,7 @@ const rocket = {
     body: new Path2D('M Z')
 };
 */
+import Rocket from "./rocket.js";
 
 $(function () {
     //const canvas = document.getElementById('canvas');//$('#canvas');
@@ -27,65 +28,24 @@ class Game {
         this.rocket = new Rocket(this);
         this.asteroids = [];
         this.timer = {};
-        setInt(this.draw.bind(this), period, this.timer);
+        setInterval(this.draw.bind(this), period);
+        window.onkeypress = ev => {
+            switch (ev.key) {
+                case 'a':
+                    this.rocket.turn(-0.5);
+                    break;
+                case 'd':
+                    this.rocket.turn(0.5);
+                    break;
+            }
+        }
     }
 
     draw() {
-        this.rocket.draw(this.ctx);
+        this.ctx.clearRect(0,0,600,800);
+        this.rocket.draw();
     }
 
 }
 
-class IDrawable {
-    draw(ctx) {}
-}
 
-class Rocket extends IDrawable {
-    constructor(game) {
-        super();
-        this.game = game;
-    }
-    x = 0;
-    y = 0;
-    velocity = 0;
-    angle = 0;
-    draw(ctx = this.game.ctx) {
-        //left engine
-        ctx.beginPath();
-        ctx.fillStyle = '#FF1111';
-        ctx.moveTo(this.x + 0, this.y + 180);
-        ctx.lineTo(this.x + 15, this.y + 150);
-        ctx.lineTo(this.x + 15, this.y + 50);
-        ctx.lineTo(this.x + 0, this.y + 80);
-        ctx.fill();
-        //right engine
-        ctx.beginPath();
-        ctx.moveTo(this.x + 80, this.y + 180);
-        ctx.lineTo(this.x + 65, this.y + 150);
-        ctx.lineTo(this.x + 65, this.y + 50);
-        ctx.lineTo(this.x + 80, this.y + 80);
-        ctx.fill();
-        //body
-        ctx.beginPath();
-        ctx.fillStyle = '#050505';
-        ctx.moveTo(this.x + 40, this.y + 0);
-        ctx.lineTo(this.x + 65, this.y + 25);
-        ctx.lineTo(this.x + 65, this.y + 150);
-        ctx.lineTo(this.x + 15, this.y + 150);
-        ctx.lineTo(this.x + 15, this.y + 25);
-        ctx.fill();
-    }
-
-    move() {
-        this.x += this.velocity * Math.cos(this.angle);
-        this.y += this.velocity * Math.sin(this.angle);
-    }
-
-    act(iDrawable = undefined) {
-        this.move();
-    }
-
-    shoot() {
-
-    }
-}
